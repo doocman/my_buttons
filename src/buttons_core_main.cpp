d5413d2ff4455143d81b6257f2cc25835322bb25
doocman <robin.savonensoderholm@gmail.com>
2024-12-16 13:42:44 +0100

    encapsulate pwm fader

--- src/buttons_core_main.cpp
+++ src/buttons_core_main.cpp
@@ -1,12 +1,13 @@
 
+#include <algorithm>
+#include <array>
 #include <chrono>
 #include <thread>
-#include <algorithm>
 
 #include "pico/stdlib.h"
-#include <hardware/pwm.h>
 #include <hardware/adc.h>
 #include <hardware/dma.h>
+#include <hardware/pwm.h>
 
 #include <picolinux/picolinux_libc.hpp>
 
@@ -14,8 +15,8 @@
 
 #include <class/cdc/cdc_device.h>
 #include <device/usbd.h>
-#include <fmt/core.h>
 #include <fmt/chrono.h>
+#include <fmt/core.h>
 #include <pico/bootrom.h>
 #include <pico/stdlib.h>
 
@@ -33,75 +34,122 @@ template <ct_int pin> struct pico_toggle_gpio {
   }
 };
 
-template <ct_int adc_pin, std::size_t buff_size> adc2dma {
-    std::array<std::uint16_t, buff_size * 2> tot_buff_;
-    uint dma_chan_;
-    dma_channel_config cfg_;
-    bool read_first_ = false;
+template <ct_int adc_pin, std::size_t buff_size> class adc2dma {
+  std::array<std::uint16_t, buff_size * 2> tot_buff_;
+  uint dma_chan_;
+  dma_channel_config cfg_;
+  bool read_first_ = false;
 
-    static constexpr auto adc_channel = adc_pin.i - 26;
+  static constexpr auto adc_channel = adc_pin.i - 26;
+
+  template <bool is_read>
+  constexpr std::span<std::uint16_t, buff_size> get_buff() {
+    if (is_read == read_first_) {
+      return std::span<std::uint16_t, buff_size>{tot_buff_.data(), buff_size};
+    } else {
+      return std::span<std::uint16_t, buff_size>{
+          tot_buff_.data() + static_cast<std::ptrdiff_t>(buff_size), buff_size};
+    }
+  }
+
+  constexpr std::span<std::uint16_t, buff_size> write_buff() {
+    return get_buff<false>();
+  }
+  constexpr std::span<std::uint16_t, buff_size> read_buff() {
+    return get_buff<true>();
+  }
 
 public:
-  // simply because the buffer must be in memory. Could be other ways to do this.
-  adc2dma(adc2dma const&) = delete;
-  adc2dma& operator=(adc2dma const&) = delete;
+  // simply because the buffer must be in memory. Could be other ways to do
+  // this.
+  adc2dma(adc2dma const &) = delete;
+  adc2dma &operator=(adc2dma const &) = delete;
+  adc2dma() = default;
 
   void init() {
-    
-  adc_init();
 
-  // Make sure GPIO is high-impedance, no pullups etc
-  adc_gpio_init(adc_pin.i);
-  // Select ADC input 0 (GPIO26)
-  adc_select_input(adc_channel);
-  adc_set_clkdiv(0);
-  adc_fifo_setup(
-        true,    // Write each completed conversion to the sample FIFO
-        true,    // Enable DMA data request (DREQ)
-        1,       // DREQ (and IRQ) asserted when at least 1 sample present
-        false,   // We won't see the ERR bit because of 8 bit reads; disable.
-        false     // Shift each sample to 8 bits when pushing to FIFO
+    adc_init();
+
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(adc_pin.i);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(adc_channel);
+    adc_set_clkdiv(0);
+    adc_fifo_setup(
+        true,  // Write each completed conversion to the sample FIFO
+        true,  // Enable DMA data request (DREQ)
+        1,     // DREQ (and IRQ) asserted when at least 1 sample present
+        false, // We won't see the ERR bit because of 8 bit reads; disable.
+        false  // Shift each sample to 8 bits when pushing to FIFO
     );
-    
-    dma_chan = dma_claim_unused_channel(true);
-    cfg = dma_channel_get_default_config(dma_chan);
-    
+
+    dma_chan_ = dma_claim_unused_channel(true);
+    cfg_ = dma_channel_get_default_config(dma_chan_);
+
     // Reading from constant address, writing to incrementing byte addresses
-    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
-    channel_config_set_read_increment(&cfg, false);
-    channel_config_set_write_increment(&cfg, true);
+    channel_config_set_transfer_data_size(&cfg_, DMA_SIZE_16);
+    channel_config_set_read_increment(&cfg_, false);
+    channel_config_set_write_increment(&cfg_, true);
 
     // Pace transfers based on availability of ADC samples
-    channel_config_set_dreq(&cfg, DREQ_ADC);
-    
+    channel_config_set_dreq(&cfg_, DREQ_ADC);
+
     auto capture_buf = write_buff();
-    dma_channel_configure(dma_chan, &cfg,
-        capture_buf.data(),    // dst
-        &adc_hw->fifo,  // src
-        capture_buf.size(),  // transfer count
-        true            // start immediately
+    dma_channel_configure(dma_chan_, &cfg_,
+                          capture_buf.data(), // dst
+                          &adc_hw->fifo,      // src
+                          capture_buf.size(), // transfer count
+                          true                // start immediately
     );
     adc_run(true);
   }
-  bool is_adc_ready() {
-    return !dma_channel_is_busy(dma_chan);
-  }
+  bool is_adc_ready() { return !dma_channel_is_busy(dma_chan_); }
   auto read_averaged_adc() {
-    read_is_first = !read_is_first;
+    read_first_ = !read_first_;
     auto to_read = read_buff();
     auto capture_buf = write_buff();
-    dma_channel_configure(dma_chan, &cfg,
-        capture_buf.data(),    // dst
-        &adc_hw->fifo,  // src
-        capture_buf.size(),  // transfer count
-        true            // start immediately
+    dma_channel_configure(dma_chan_, &cfg_,
+                          capture_buf.data(), // dst
+                          &adc_hw->fifo,      // src
+                          capture_buf.size(), // transfer count
+                          true                // start immediately
     );
-    auto res_sum = *std::ranges::fold_left(to_read, std::plus<>{}, std::int_fast32_t{});
+    auto res_sum =
+        std::ranges::fold_left(to_read, std::int_fast32_t{}, std::plus<>{});
     return res_sum / buff_size;
   }
   void sleep() {
     adc_run(false);
     adc_fifo_drain();
+  }
+};
+
+template <ct_int pin, uint resolution>
+  requires(resolution > 1)
+class pwm_led_fader {
+public:
+  static void init() {
+    gpio_set_function(pin.i, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice is connected to GPIO pin
+    uint slice_num = pwm_gpio_to_slice_num(pin.i);
+    uint channel = pwm_gpio_to_channel(pin.i);
+
+    // Set period of resolution cycles (is inclusive)
+    pwm_set_wrap(slice_num, resolution - 1);
+    // Set channel A output high for one cycle before dropping
+    pwm_set_chan_level(slice_num, channel, 1);
+    // Set the PWM running
+    pwm_set_enabled(slice_num, true);
+  }
+  static void set_level(uint l) {
+    uint slice_num = pwm_gpio_to_slice_num(pin.i);
+    uint channel = pwm_gpio_to_channel(pin.i);
+    pwm_set_chan_level(slice_num, channel, l);
+  }
+  static void sleep() {
+    uint slice_num = pwm_gpio_to_slice_num(pin.i);
+    pwm_set_enabled(slice_num, false);
   }
 };
 
@@ -123,7 +171,8 @@ static auto context = ui_context::builder()
                               )
                           .build();
 
-static auto the_adc = adc2dma<26, 512>;
+static auto the_adc = adc2dma<26, 512>{};
+using the_fader_t = pwm_led_fader<25, 256>;
 
 void main() {
   stdio_init_all();
@@ -133,20 +182,9 @@ void main() {
     }
   }
 
-  //setup_adc();
+  // setup_adc();
   the_adc.init();
-  gpio_set_function(25, GPIO_FUNC_PWM);
-
-  // Find out which PWM slice is connected to GPIO 25
-  uint slice_num = pwm_gpio_to_slice_num(25);
-  uint channel = pwm_gpio_to_channel(25);
-
-  // Set period of 4 cycles (0 to 3 inclusive)
-  pwm_set_wrap(slice_num, 255);
-  // Set channel A output high for one cycle before dropping
-  pwm_set_chan_level(slice_num, channel, 1);
-  // Set the PWM running
-  pwm_set_enabled(slice_num, true);
+  the_fader_t::init();
 
   while (tud_cdc_n_write_flush(0) != 0) {
     std::this_thread::sleep_for(std::chrono::milliseconds(5));
@@ -160,33 +198,34 @@ void main() {
   nanoseconds time_waited{};
   nanoseconds time_calculated{};
   while (stdio_usb_connected()) {
-    //std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
     auto org_tp = steady_clock::now();
-    //wait_for_adc();
-    while(!adc2dma.is_adc_ready()) {}
+    // wait_for_adc();
+    while (!the_adc.is_adc_ready()) {
+    }
     auto wait_end = steady_clock::now();
-    adc_sum += adc2dma.read_averaged_adc();
+    adc_sum += the_adc.read_averaged_adc();
     ++num_adc_vals;
     now = steady_clock::now();
     time_waited += (wait_end - org_tp);
     time_calculated += (now - wait_end);
     if (now >= next_print) {
-        ++cur_pwm_level;
-        cur_pwm_level = cur_pwm_level % 256;
-        pwm_set_chan_level(slice_num, channel, cur_pwm_level);
-        // std::cout << "set pwm level " << cur_pwm_level << '\n';
-        fmt::print("Set pwm level to {}\n", cur_pwm_level);
-        if (num_adc_vals != 0) {
-          fmt::print("ADC value was {}\n", adc_sum / num_adc_vals);
-            adc_sum = 0;
-            num_adc_vals = 0;
-        } else {
-            fmt::print("NO ADC VALUE WAS RECEIVED!\n");
-        }
-        fmt::print("Time waited: {} and time calculated: {}\n", time_waited, time_calculated);
-        time_waited = 0ns;
-        time_calculated = 0ns;
-        next_print = now + 1s;
+      ++cur_pwm_level;
+      cur_pwm_level = cur_pwm_level % 256;
+      the_fader_t::set_level(cur_pwm_level);
+      fmt::print("Set pwm level to {}\n", cur_pwm_level);
+      if (num_adc_vals != 0) {
+        fmt::print("ADC value was {}\n", adc_sum / num_adc_vals);
+        adc_sum = 0;
+        num_adc_vals = 0;
+      } else {
+        fmt::print("NO ADC VALUE WAS RECEIVED!\n");
+      }
+      fmt::print("Time waited: {} and time calculated: {}\n", time_waited,
+                 time_calculated);
+      time_waited = 0ns;
+      time_calculated = 0ns;
+      next_print = now + 1s;
     }
   }
   tud_disconnect();
